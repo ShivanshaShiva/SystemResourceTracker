4606687a990ea8fa749da56bd7f7279e15c6df1b
ShivanshaShiva <nkg7060@gmail.com>
2025-05-08 11:22:11 +0000

    Pre-Recovery checkpoint

--- replit.nix
+++ replit.nix
@@ -1,11 +1,13 @@
 {pkgs}: {
   deps = [
+    pkgs.haskellPackages.streamly-statistics
+    pkgs.streamlit
+    pkgs.python311Packages.streamlit
     pkgs.python311Packages.google-cloud-core
     pkgs.python311Packages.google-cloud-workflows
     pkgs.deepin.dwayland
     pkgs.sbclPackages.qtools-ui-spellchecked-text-edit
     pkgs.python311Packages.qiskit-terra
     pkgs.python311Packages.qiskit-aer
-    pkgs.python312Packages.qiskit-terra
   ];
 }
